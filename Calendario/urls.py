733b62da1fd72ab0cd0ba96835bd4d7f1f839b76
Vitor Rezende <53321198+vterez@users.noreply.github.com>
2022-07-07 19:17:49 -0300

    Add files via upload

---
+++ Calendario/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('',Inicial,name='Inicial'),
+    path('Confirm',Confirm,name='Confirm'),
+    path('Upload',Upload,name='Upload'),
+    path('Uploaded',Uploaded,name='Uploaded'),
+    path('ValidaCodigo',ValidaCodigo,name='ValidaCodigo'),
+]
---
+++ Calendario/views.py
@@ -0,0 +1,134 @@
+from .models import *
+from django.shortcuts import render,redirect
+from django.http import HttpResponse, JsonResponse, FileResponse
+from hashlib import blake2s as khash
+from django.db import IntegrityError
+from django.utils.timezone import now
+import csv
+from datetime import datetime
+from weasyprint import HTML
+from django.template.loader import render_to_string
+
+def msort(e):
+    return e[0]
+
+def OrganizaHorarios():
+    horarios = HorarioLivre.objects.values_list("id","horario")
+    horarios_formatados = []
+    for i in range(len(horarios)):
+        horarios_formatados.append((horarios[i][0],horarios[i][1].strftime("Dia %d/%m às %H:%M")))
+    return horarios_formatados
+
+def decode_utf8(input_iterator):
+    """Função decodificadora para abertura dos arquivos csv"""
+    for l in input_iterator:
+        yield l.decode('utf-8')
+        
+def Inicial(request):
+    horarios = sorted(OrganizaHorarios(),key=msort)
+    if horarios:
+        return render(request,'myform.html',{"failed":"","horarios":horarios})
+    else:
+        return render(request,'failed.html',{'failed':'Não há horários disponíveis. Favor contatar o Pedro.'})
+
+def Confirm(request):
+    post = request.POST
+    obs = ""
+    if 'opcao' not in post:
+        horarios = sorted(OrganizaHorarios(),key=msort)
+        return render(request,'myform.html',{"failed":"Horário indisponível. Alguém selecionou esse horário enquanto você decidia. Por favor, selecione outro","horarios":horarios})
+    opcao = post["opcao"]
+    matricula = post['matricula']
+    try:
+        mat = Matricula.objects.get(matricula=matricula)
+    except:
+        horarios = sorted(OrganizaHorarios(),key=msort)
+        return render(request,'myform.html',{'failed':f"Número de matrícula não cadastrado na turma. O número informado foi {matricula}","horarios":horarios
+        })
+    try:
+        livre = HorarioLivre.objects.get(id=opcao)
+        marcar = livre.horario
+        num = livre.id
+        marcado = HorarioMarcado(opcao,marcar,post["matricula"],post['nome'],post["email"],num)
+        marcado.save()
+        livre.delete()
+    except HorarioLivre.DoesNotExist:
+        horarios = sorted(OrganizaHorarios(),key=msort)
+        if horarios:
+            return render(request,'myform.html',{"failed":"Horário indisponível. Alguém selecionou esse horário enquanto você decidia. Por favor, selecione outro","horarios":horarios})
+        else:
+            return render(request,'failed.html',{'failed':'Não há horários disponíveis. Favor contatar o Pedro.'})
+    except IntegrityError:
+        antigo = HorarioMarcado.objects.get(matricula=matricula)
+        dt = antigo.horario
+        id = antigo.num
+        antigo.delete()
+        livre.delete()
+        livre = HorarioLivre(dt,id)
+        marcado.save()
+        livre.save()
+        obs = "Já tinha sido selecionado um horário para seu número de matrícula, então, ele foi liberado e o novo horário foi alocado"
+        
+    except Exception as ex:
+        return render(request,'failed.html',{"failed":"Erro inesperado, por favor, tente novamente. Caso não consiga, entre em contato com o Pedro","ex":ex})
+    mystr = f'{post["matricula"]}_{post["opcao"]}'.encode('utf8')
+    val = str(khash(mystr).hexdigest())[:30]
+    hora = f'{marcado.horario.hour:02d}:{marcado.horario.minute:02d}'
+    dia = f'{marcado.horario.day:02d}/{marcado.horario.month:02d}'
+    html = render_to_string('code.html',{"opcao":opcao,"obs":obs,"code":val,'hora':hora,'dia':dia,'nome':post['nome'],'matricula':matricula})
+    pdf = HTML(string=html).write_pdf()
+    with open('confirmacao.pdf', 'wb') as arq:
+        arq.write(pdf)
+    return FileResponse(open('confirmacao.pdf', 'rb'))
+
+def Upload(request):
+    return render(request,'upload.html',{'data':'','erro':''})
+
+def Uploaded(request):
+    if request.POST['senha'] != "pedrolmo":
+        return render(request,'upload.html',{'data':f'{0} entradas foram adicionadas','erro':'Senha inválida'})
+    reader = csv.reader(decode_utf8(request.FILES['csvfile']))
+    if request.POST['opcao'] == 'Matrículas':
+        erro = []
+        j=0
+        for i in reader:
+            try:
+                matricula = Matricula(matricula=int(i[0]))
+                matricula.save()
+                j+=1
+            except BaseException as ex:
+                print(ex)
+                erro.append(i[0])
+
+    else:
+        year = now().year
+        j=0
+        erro = []
+        for n,i in enumerate(reader,len(HorarioLivre.objects.all())):
+            try:
+                if len(i) == 2:
+                    day,month = map(int,i[0].split('/'))
+                    hour,minute = map(int,i[1].split(':'))
+                    dt = datetime(hour=hour,minute=minute,month=month,day=day,year=year,second=0)
+                    livre = HorarioLivre(horario=dt,id=n)
+                    livre.save()
+                    j+=1
+                else:
+                    erro.append(str(n+1))
+            except:
+                erro.append(str(n+1))
+        
+    return render(request,'upload.html',{'data':f'{j} entradas foram adicionadas','erro':';'.join(erro)})
+
+def ValidaCodigo(request):
+    resp = ''
+    if 'matricula' in request.GET:
+        matricula = request.GET['matricula']
+        opcao = request.GET['opcao']
+        codigo = request.GET['codigo']
+        mystr = f'{matricula}_{opcao}'.encode('utf8')
+        val = str(khash(mystr).hexdigest())[:30]
+        resp = f"Código {'válido' if val == codigo else 'inválido'}"
+    return render(request,'valida.html', {'resp':resp})
+    
+    
